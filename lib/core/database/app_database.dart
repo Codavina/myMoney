27726438fa90db68f0e0c3ad5a35d70e16038088
Oussama ZED <contact.codavina@gmail.com>
2026-07-01 18:12:01 +0100

    create sqlite database, tables, indexes and triggers

---
+++ lib/core/database/app_database.dart
@@ -0,0 +1,58 @@
+import 'dart:developer';
+import 'package:my_money/core/database/database_schema.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class AppDatabase {
+  // Singleton instance → ensures only ONE database connection exists
+  static final AppDatabase instance = AppDatabase._internal();
+  static Database? _db;
+
+  // Private constructor → prevents creating multiple instances
+  AppDatabase._internal();
+
+  // Public getter for database instance
+  // Opens database only once and reuses it (performance optimization)
+  Future<Database> get database async {
+    if (_db != null) return _db!;
+    _db = await _initDB();
+    return _db!;
+  }
+
+  // Create or open the database file
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'my_money.db');
+
+    // [version : 1] Database version → used for migrations later
+    // [onCreate] Called only the FIRST time database is created
+    return openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+      onConfigure: _configureDB,
+      onUpgrade: _upgradeDB
+    );
+  }
+
+  // Create tables when database is first created
+  Future<void> _createDB(Database db, int version) async {
+    await DatabaseSchema.create(db);
+    log("On Create ==========================");
+  }
+
+
+  // Configure the database before it is opened.
+// This is called every time the database is opened.
+// Used here to enable foreign key constraints in SQLite.
+  Future<void> _configureDB(Database db) async {
+    await db.execute('PRAGMA foreign_keys = ON');
+    log("On Configure ==========================");
+  }
+
+  // Handles database schema changes when upgrading to a newer version.
+// Add migration logic here to preserve existing user data.
+  Future<void> _upgradeDB(Database db,int oldVersion,int newVersion,) async {
+    log("On Upgrade ==========================");
+  }
+}
---
+++ lib/core/database/database_schema.dart
@@ -0,0 +1,167 @@
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseSchema {
+
+  static Future<void> create(Database db) async {
+
+    ///Create Tables
+    await _createCurrenciesTable(db);
+    await _createFundsTable(db);
+    await _createTransactionsTable(db);
+
+    ///Create Indexes
+    await _createIndexes(db);
+
+    ///Create Triggers
+    await _createTriggers(db);
+
+
+  }
+
+  // ===========================
+  // Tables
+  // ===========================
+
+  static Future<void> _createCurrenciesTable(Database db) async {
+    await db.execute('''
+      CREATE TABLE Currencies(
+        currency_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        currency_code TEXT NOT NULL UNIQUE
+          CHECK(length(currency_code) BETWEEN 3 AND 10)
+      );
+    ''');
+  }
+
+  static Future<void> _createFundsTable(Database db) async {
+    await db.execute('''
+      CREATE TABLE Funds(
+        fund_id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        title TEXT NOT NULL UNIQUE,
+
+        balance REAL NOT NULL
+          DEFAULT 0
+          CHECK(balance >= 0),
+
+        currency_id INTEGER NOT NULL,
+
+        is_archived INTEGER NOT NULL
+          DEFAULT 0
+          CHECK(is_archived IN (0,1)),
+
+        created_at TEXT NOT NULL
+          DEFAULT CURRENT_TIMESTAMP,
+
+        FOREIGN KEY(currency_id)
+          REFERENCES Currencies(currency_id)
+          ON UPDATE CASCADE
+          ON DELETE RESTRICT
+      );
+    ''');
+  }
+
+  static Future<void> _createTransactionsTable(Database db) async {
+    await db.execute('''
+      CREATE TABLE Transactions(
+        transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        fund_id INTEGER NOT NULL,
+
+        amount REAL NOT NULL
+          CHECK(amount > 0),
+
+        transaction_type INTEGER NOT NULL
+          CHECK(transaction_type IN (0,1)),
+
+        transaction_date TEXT NOT NULL,
+
+        description TEXT,
+
+        created_at TEXT NOT NULL
+          DEFAULT CURRENT_TIMESTAMP,
+
+        FOREIGN KEY(fund_id)
+          REFERENCES Funds(fund_id)
+          ON UPDATE CASCADE
+          ON DELETE RESTRICT
+      );
+    ''');
+  }
+
+  // ===========================
+  // Indexes
+  // ===========================
+
+  static Future<void> _createIndexes(Database db) async {
+    await db.execute('''
+      CREATE INDEX IX_Transactions_Fund
+      ON Transactions(fund_id);
+    ''');
+
+    await db.execute('''
+      CREATE INDEX IX_Transactions_Date
+      ON Transactions(transaction_date);
+    ''');
+  }
+
+  // ===========================
+  // Triggers
+  // ===========================
+
+  static Future<void> _createTriggers(Database db) async {
+
+    // ==========================================================
+    // 1. Update Fund Balance After Insert Transaction
+    // ==========================================================
+    await db.execute('''
+    CREATE TRIGGER trg_InsertTransaction
+    AFTER INSERT ON Transactions
+    BEGIN
+      UPDATE Funds
+      SET balance = balance +
+      CASE
+        WHEN NEW.transaction_type = 0 THEN NEW.amount
+        ELSE -NEW.amount
+      END
+      WHERE fund_id = NEW.fund_id;
+    END;
+  ''');
+
+    // ==========================================================
+    // 2. Restore Fund Balance After Delete Transaction
+    // ==========================================================
+    await db.execute('''
+    CREATE TRIGGER trg_DeleteTransaction
+    AFTER DELETE ON Transactions
+    BEGIN
+      UPDATE Funds
+      SET balance = balance +
+      CASE
+        WHEN OLD.transaction_type = 0 THEN -OLD.amount
+        ELSE OLD.amount
+      END
+      WHERE fund_id = OLD.fund_id;
+    END;
+  ''');
+
+    // ==========================================================
+    // 3. Prevent Withdraw If Balance Is Insufficient
+    // ==========================================================
+    await db.execute('''
+    CREATE TRIGGER trg_CheckBalanceBeforeInsert
+    BEFORE INSERT ON Transactions
+    WHEN NEW.transaction_type = 1
+    BEGIN
+      SELECT CASE
+        WHEN (
+          SELECT balance
+          FROM Funds
+          WHERE fund_id = NEW.fund_id
+        ) < NEW.amount
+        THEN RAISE(ABORT, 'Insufficient balance')
+      END;
+    END;
+  ''');
+
+  }
+}
